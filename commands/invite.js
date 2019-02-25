51da79f516416c09585fda055b882532fbe74a3e
AnatoleLucet <lucet.anatole@gmail.com>
2019-02-25 12:49:23 +0100

    updating invite link (again)

--- commands/invite.js
+++ commands/invite.js
@@ -1,3 +1,3 @@
 exports.run = (client, message) => {
-  message.channel.send('https://discordapp.com/oauth2/authorize?client_id=514793955745857536&scope=bot%20identify&permissions=104127488');
+  message.channel.send('https://discordapp.com/oauth2/authorize?client_id=514793955745857536&scope=bot&permissions=104127488');
 };
