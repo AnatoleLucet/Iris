80f974d5f3f5e3b8cb0fc2298ffb2bc4af100a48
AnatoleLucet <lucet.anatole@gmail.com>
2019-03-11 18:01:52 +0100

    fixing the 'non activity' issue after multiple days of bot serving

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,12 @@
 module.exports = (client) => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity(`${process.env.CLIENT_PREFIX}help`);
+
+  const activity = () => {
+    client.user.setActivity(`${process.env.CLIENT_PREFIX}help`);
+  };
+  activity();
+
+  setInterval(() => {
+    activity();
+  }, 1000 * 60 * 60 * 24);
 };
